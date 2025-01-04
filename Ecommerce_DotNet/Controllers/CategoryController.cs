edae95e85d997697255145dc1e016786d5f0ce53
bips55 <subedi.biplove10@gmail.com>
2025-01-04 16:06:56 +0545

    getting Data @Category and displaying in action method

--- Ecommerce_DotNet/Controllers/CategoryController.cs
+++ Ecommerce_DotNet/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecommerce_DotNet.Data;
+using Ecommerce_DotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce_DotNet.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objectCategoryList = _context.Categories.ToList();
+            return View(objectCategoryList);
         }
     }
 }
