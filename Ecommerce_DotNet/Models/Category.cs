e099b4886784e81996009bbc0e87ca93b773629c
bips55 <subedi.biplove10@gmail.com>
2025-01-04 18:34:10 +0545

    Client side and server side validations added for creating Category

--- Ecommerce_DotNet/Models/Category.cs
+++ Ecommerce_DotNet/Models/Category.cs
@@ -9,9 +9,12 @@ namespace Ecommerce_DotNet.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(20)]
         public string Name  { get; set; }
         [Required]
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1 to 100")]
         public int DisplayOrder { get; set; }
     }
 }
+
