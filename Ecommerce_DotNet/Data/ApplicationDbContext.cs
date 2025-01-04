9a552fb7815e2d3c593651ff5e8e4496f25b52ba
bips55 <subedi.biplove10@gmail.com>
2025-01-04 15:48:52 +0545

    Category Data Seeded

--- Ecommerce_DotNet/Data/ApplicationDbContext.cs
+++ Ecommerce_DotNet/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace Ecommerce_DotNet.Data
             
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                  new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                    new Category { Id = 3, Name = "Romance", DisplayOrder = 3 }
+                );
+        }
     }
 }
