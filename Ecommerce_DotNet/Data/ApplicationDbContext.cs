01f3cadef9a969aaa739fad94ce10b19e771c58e
BIPLOV <BIPLOV@DESKTOP-DG879QN>
2025-01-04 14:51:48 +0545

    ApplicationDbContext Configured

---
+++ Ecommerce_DotNet/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_DotNet.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+            
+        }
+    }
+}
