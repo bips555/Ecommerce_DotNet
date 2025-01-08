ec8c5b21be8d805c10875c9a8d83600310cf9182
BIPLOV <BIPLOV@DESKTOP-DG879QN>
2025-01-08 21:11:06 +0545

    VIewModels used for implementing strongly typed views for Product View

--- Ecommerce_Models/Product.cs
+++ Ecommerce_Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -39,6 +40,7 @@ namespace Ecommerce_Models
         public double Price100 { get; set; }
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
+        [ValidateNever]
         public Category Category { get; set; }
         public string ImageUrl { get; set; }
 
