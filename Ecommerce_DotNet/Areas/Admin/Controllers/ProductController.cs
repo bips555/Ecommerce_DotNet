6345fdcd51a93df4ab21697f7db9a41646ad1606
bips55 <subedi.biplove10@gmail.com>
2025-01-08 18:29:44 +0545

    VIew And Controller Coded For Product

---
+++ Ecommerce_DotNet/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿using Ecommerce_DataAccess.Data;
+using Ecommerce_DataAccess.Repository.IRepository;
+using Ecommerce_Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_DotNet.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objectProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objectProductList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product Product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(Product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Added Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product ProductById = _unitOfWork.Product.Get(u => u.Id == id);
+            if (ProductById == null)
+            {
+                return NotFound();
+            }
+            return View(ProductById);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product Product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(Product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product ProductById = _unitOfWork.Product.Get(u => u.Id == id);
+            if (ProductById == null)
+            {
+                return NotFound();
+            }
+            return View(ProductById);
+
+
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product Product = _unitOfWork.Product.Get(u => u.Id == id);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(Product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted Successfully";
+
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
