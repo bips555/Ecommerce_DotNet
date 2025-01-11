61dbfe8991354a2034c156ccc41dc5915ccd3398
BIPLOV <BIPLOV@DESKTOP-DG879QN>
2025-01-11 17:25:01 +0545

    Roles created for the user by programming program.cs and adding an default implementation for IEmailSender interface

--- Ecommerce_Utilities/SD.cs
+++ Ecommerce_Utilities/SD.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Ecommerce_Utility
 {
-    internal class SD
+    public static class SD
     {
+        public const string Role_Customer = "Customer";
+
+        public const string Role_Company = "Company";
+
+        public const string Role_Admin = "Admin";
+
+        public const string Role_Employee = "Employee";
+
     }
 }
