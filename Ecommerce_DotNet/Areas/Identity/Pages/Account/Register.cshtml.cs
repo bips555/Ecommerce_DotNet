7a20020e96f5b9ffea247f1b3436d6f22204ff5f
BIPLOV <BIPLOV@DESKTOP-DG879QN>
2025-01-11 21:19:28 +0545

    Company Foreign Key Established to User and company toggle option added in user registration

--- Ecommerce_DotNet/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Ecommerce_DotNet/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using Ecommerce_DataAccess.Repository.IRepository;
 using Ecommerce_Models;
 using Ecommerce_Utility;
 using Microsoft.AspNetCore.Authentication;
@@ -35,6 +36,7 @@ namespace Ecommerce_DotNet.Areas.Identity.Pages.Account
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -42,7 +44,8 @@ namespace Ecommerce_DotNet.Areas.Identity.Pages.Account
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
             RoleManager<IdentityRole> roleManager,
-            IEmailSender emailSender) 
+            IEmailSender emailSender,
+            IUnitOfWork unitOfWork) 
         {
             _roleManager = roleManager;
             _userManager = userManager;
@@ -51,6 +54,7 @@ namespace Ecommerce_DotNet.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _unitOfWork = unitOfWork;
         }
 
         /// <summary>
@@ -117,6 +121,10 @@ namespace Ecommerce_DotNet.Areas.Identity.Pages.Account
             public string? State { get; set; }
             public string? PostalCode { get; set; }
             public string? PhoneNumber { get; set; }
+
+            public int? CompanyId { get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
         }
 
 
@@ -135,6 +143,11 @@ namespace Ecommerce_DotNet.Areas.Identity.Pages.Account
                 {
                     Text = i,
                     Value = i
+                }),
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 })
             };
             ReturnUrl = returnUrl;
@@ -157,6 +170,10 @@ namespace Ecommerce_DotNet.Areas.Identity.Pages.Account
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber= Input.PhoneNumber;
                 user.State = Input.State;
+                if(Input.Role==SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
