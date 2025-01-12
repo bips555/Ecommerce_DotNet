917dee8fae9d61cdd92276c7bebff6995d577c42
BIPLOV <BIPLOV@DESKTOP-DG879QN>
2025-01-12 19:12:50 +0545

    Repository and unit of work configured for ShoppingCart

---
+++ Ecommerce_DataAccess/Repository/ShoppingRepository.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce_DataAccess.Data;
+using Ecommerce_DataAccess.Repository.IRepository;
+using Ecommerce_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ShoppingCartRepository(ApplicationDbContext context):base(context) 
+        {
+            _context = context;
+        }
+
+        public void Update(ShoppingCart shoppingCart)
+        {
+           _context.ShoppingCarts.Update(shoppingCart);
+        }
+    }
+}
