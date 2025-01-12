6688e0cb0031d1334ac6d849f95cfcab29e61ae6
BIPLOV <BIPLOV@DESKTOP-DG879QN>
2025-01-12 19:04:19 +0545

    Shopping Cart Model added With ProductId and ApplicationUserId as Foreign Key

--- Ecommerce_DataAccess/Data/ApplicationDbContext.cs
+++ Ecommerce_DataAccess/Data/ApplicationDbContext.cs
@@ -16,9 +16,9 @@ namespace Ecommerce_DataAccess.Data
        public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts  { get; set; }
 
-        
-          protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
          {
             base.OnModelCreating(modelBuilder);
              modelBuilder.Entity<Category>().HasData(
