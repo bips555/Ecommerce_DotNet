9098fc5716f34baa9ac87f16efe576f8857e52ef
BIPLOV <subedi.biplove10@gmail.com>
2025-01-18 19:37:48 +0545

    constants for order status and payment status defined in SD.cs in Utilities

--- Ecommerce_Utilities/SD.cs
+++ Ecommerce_Utilities/SD.cs
@@ -15,6 +15,17 @@ namespace Ecommerce_Utility
         public const string Role_Admin = "Admin";
 
         public const string Role_Employee = "Employee";
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
 
     }
 }
