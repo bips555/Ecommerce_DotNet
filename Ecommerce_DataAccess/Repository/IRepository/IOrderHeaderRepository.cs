5bc49f673fd76ecf24e2e8e32f788c171babdd22
BIPLOV <subedi.biplove10@gmail.com>
2025-01-19 13:30:54 +0545

    Session Id added in model and order header repository helper methods added to update order status payment status session Id and payment Intent Id

--- Ecommerce_DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ Ecommerce_DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -10,6 +10,8 @@ namespace Ecommerce_DataAccess.Repository.IRepository
     public interface IOrderHeaderRepository:IRepository<OrderHeader>
     {
         void Update(OrderHeader orderHeader);
-      
+        void UpdateStatus(int id, string orderStatus,string paymentStatus);
+        void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId);
+
     }
 }
