db127a1ac3188c5a62da35f7a249bae53827f0bc
BIPLOV <subedi.biplove10@gmail.com>
2025-01-20 16:25:51 +0545

    Orders in ordercontroller filtered according to order status

--- Ecommerce_DotNet/Areas/Admin/Controllers/OrderController.cs
+++ Ecommerce_DotNet/Areas/Admin/Controllers/OrderController.cs
@@ -1,10 +1,13 @@
 ﻿using Ecommerce_DataAccess.Repository.IRepository;
 using Ecommerce_Models;
+using Ecommerce_Utility;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Diagnostics;
 
 namespace Ecommerce_DotNet.Areas.Admin.Controllers
 {
-    [Area("Admin")] 
+    [Area("admin")] 
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -17,10 +20,29 @@ namespace Ecommerce_DotNet.Areas.Admin.Controllers
             return View();
         }
         [HttpGet]
-        public IActionResult Getall()
+        public IActionResult Getall(string status)
         {
-            List<OrderHeader> objectOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objectOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            switch (status)
+            {
+                case "pending":
+                    objectOrderHeader = objectOrderHeader.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objectOrderHeader = objectOrderHeader.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objectOrderHeader = objectOrderHeader.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objectOrderHeader = objectOrderHeader.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
             return Json(new { data = objectOrderHeader });
+
+           
         }
     }
 }
--- Ecommerce_DotNet/wwwroot/js/order.js
+++ Ecommerce_DotNet/wwwroot/js/order.js
@@ -1,20 +1,40 @@
 ﻿var dataTable;
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess")
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed")
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending")
+            } else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved")
+                }
+                else {
+                    loadDataTable("all")
+                }
+            }
+        }
+    }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            url: '/admin/order/getall'
+            url: '/admin/order/getall?status=' + status
         },
         "columns": [
             { data: 'id', "width": "20%" },
             { data: 'name', "width": "20%" },
             { data: 'phoneNumber', "width": "15%" },
             { data: 'applicationUser.email', "width": "15%" },
-            { data: 'orderstatus', "width": "10%" },
-            { data: 'ordertotal', "width": "15%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "15%" },
            
             {
                 data: 'id',
@@ -31,27 +51,5 @@ function loadDataTable() {
                 "width": "15%"
             }
         ]
-    });
-}
-function Delete(url) {
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: url,
-                type: "DELETE",
-                success: function (data) {
-                    dataTable.ajax.reload();
-                    toastr.success(data.message);
-                }
-            })
-        }
     });
 }
