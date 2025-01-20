94d16256af17884472d71b669f9f886c07fb368b
BIPLOV <subedi.biplove10@gmail.com>
2025-01-20 15:24:27 +0545

    OrderController created for admin purposed to make necessary order and payment status changes along with OrderVM

---
+++ Ecommerce_DotNet/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce_DataAccess.Repository.IRepository;
+using Ecommerce_Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_DotNet.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Getall()
+        {
+            List<OrderHeader> objectOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objectOrderHeader });
+        }
+    }
+}
