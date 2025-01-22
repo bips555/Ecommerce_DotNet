e3db75540d0cf83329e0e58becab5f530066ca42
BIPLOV <subedi.biplove10@gmail.com>
2025-01-22 19:32:54 +0545

    Facebook recommited services

---
+++ Ecommerce_DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,65 @@
+﻿using Ecommerce_DataAccess.Data;
+using Ecommerce_Models;
+using Ecommerce_Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce_DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _context;
+
+        public DbInitializer(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+        public void Initialize()
+        {
+            try
+            {
+                if (_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@ecommercebook.com",
+                    Email = "admin@ecommercebook.com",
+                    Name = "Admin",
+                    PhoneNumber = "9845827527",
+                    StreetAddress = "TLK-02",
+                    City = "KTM",
+                    PostalCode = "45032",
+                    State = "Bagmati"
+                }, "Admin@123").GetAwaiter().GetResult();
+                ApplicationUser user = _context.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@ecommercebook.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+            return;
+        }
+    }
+}
